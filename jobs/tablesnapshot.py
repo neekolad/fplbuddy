44e4e370ea68d8d2ded1a6ea955b3f0ca453dd34
Nikola <neekolad@gmail.com>
2025-11-29 20:03:30 +0100

    make hash out of snapshot we want to insert to db, fetch last table snapshot from db for comparison

--- jobs/tablesnapshot.py
+++ jobs/tablesnapshot.py
@@ -2,7 +2,7 @@ import sqlite3
 import json
 from app.dbinit import insert_row_snapshot, db_conn
 from app.utils.logger import logger
-
+import hashlib
 
 conn = db_conn()
 conn.row_factory = sqlite3.Row
@@ -53,12 +53,35 @@ table = sorted(
     key=lambda x: (x[1]["points"], x[1]["goals_for"] - x[1]["goals_against"]),
     reverse=True
 )
+serialized_table = json.dumps(table, indent=4)
 
-# print(json.dumps(table, indent=4))
+table_hash = hashlib.sha256(serialized_table.encode("utf-8")).hexdigest()
+print(table_hash)
+
+print(json.dumps(table, indent=4))
 cursor.execute("SELECT COALESCE(MAX(snapshot_id), 0) FROM table_snapshots")
-snapshot_id = cursor.fetchone()[0] + 1
-# print(snapshot_id)
-logger.info(f"Making snapshot with id {snapshot_id}")
+snapshot_id = cursor.fetchone()[0]
+next_snapshot_id = snapshot_id + 1
+print(snapshot_id)
+
+# fetch latest table
+last_table_sql = f"SELECT * FROM table_snapshots WHERE snapshot_id={snapshot_id}"
+print(last_table_sql)
+cursor.execute(last_table_sql)
+last_inserted_table = [dict(row) for row in cursor.fetchall()]
+
+print(last_inserted_table)
+print(json.dumps(last_inserted_table, indent=4))
+# repack the data to fit the dict above that we are going to compare it to
+
+# make hash out of it
+
+# comapare it to the one we want to insert
+
+# insert if they are different
+
+
+logger.info(f"Making snapshot with id {next_snapshot_id}")
 for idx, item in enumerate(table, start=1):
     data = {}
     goal_diff = item[1]["goals_for"] - item[1]["goals_against"]
@@ -66,7 +89,7 @@ for idx, item in enumerate(table, start=1):
     data = item[1]
     data['team_name'] = item[0]
     data["type"] = 'regular'
-    data["snapshot_id"] = snapshot_id
+    data["snapshot_id"] = next_snapshot_id
     insert_row_snapshot(data, "table_snapshots")
     # print(data)
     
