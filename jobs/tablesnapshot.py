c11e9471291d8bf2ed1213c81062ab4d10d28dd7
Nikola <neekolad@gmail.com>
2025-11-30 19:46:25 +0100

    added logic for insertion of table snapshot only if its different than previous

--- jobs/tablesnapshot.py
+++ jobs/tablesnapshot.py
@@ -7,11 +7,9 @@ import hashlib
 
 def repack_table(data):
     '''repack data fetched from db to match data we are comaparing it to'''
-    repacked_data = []
+    repacked_data = {}
     for item in data:
-        repacked_data.append([
-            item["team_name"],
-            {
+        repacked_data[item["team_name"]] = {
                 "played" : item["played"],
                 "wins" : item["wins"],
                 "draws" : item["draws"],
@@ -20,17 +18,20 @@ def repack_table(data):
                 "goals_against" : item["goals_against"],
                 "points" : item["points"]
             }
-            
-            ]
-        )
     table = sorted(
-        standings.items(),
+        repacked_data.items(),
         key=lambda x: (x[1]["points"], x[1]["goals_for"] - x[1]["goals_against"]),
         reverse=True
     )
     serialized_table = json.dumps(table, indent=4)
+    print("OLD TABLE:", serialized_table)
     return serialized_table
         
+def is_different_table(old_table, new_table):
+    '''compare 2 tables'''
+    if old_table == new_table:
+        return False
+    return True
 
 
 conn = db_conn()
@@ -75,17 +76,18 @@ for match in res.fetchall():
         standings[home]["points"] += 1
         standings[away]["points"] += 1
 
-# print(standings)
+print(standings)
 
-table = sorted(
+new_table = sorted(
     standings.items(),
     key=lambda x: (x[1]["points"], x[1]["goals_for"] - x[1]["goals_against"]),
     reverse=True
 )
-serialized_table = json.dumps(table, indent=4)
+serialized_new_table = json.dumps(new_table, indent=4)
+# print("NEW TABLE:", serialized_new_table)
 
-table_hash = hashlib.sha256(serialized_table.encode("utf-8")).hexdigest()
-print(table_hash)
+new_table_hash = hashlib.sha256(serialized_new_table.encode("utf-8")).hexdigest()
+# print("NEW TABLE HASH:", new_table_hash)
 
 # print(json.dumps(table, indent=4))
 cursor.execute("SELECT COALESCE(MAX(snapshot_id), 0) FROM table_snapshots")
@@ -105,25 +107,24 @@ repacked_last_table = repack_table(last_inserted_table)
 
 # make hash out of it
 repacked_table_hash = hashlib.sha256(repacked_last_table.encode("utf-8")).hexdigest()
-print(repacked_table_hash)
+print("OLD TABLE HASH:", repacked_table_hash)
 
 # comapare it to the one we want to insert
-if table_hash == repacked_table_hash:
-    print("THEY ARE THE SAME YAYY")
+if is_different_table(new_table_hash, repacked_table_hash):
+    # insert new updated table if they are different
+    logger.info(f"Making snapshot with id {next_snapshot_id}")
+    for idx, item in enumerate(new_table, start=1):
+        data = {}
+        goal_diff = item[1]["goals_for"] - item[1]["goals_against"]
+        item[1]["goal_difference"] = goal_diff
+        data = item[1]
+        data['team_name'] = item[0]
+        data["type"] = 'regular'
+        data["snapshot_id"] = next_snapshot_id
+        insert_row_snapshot(data, "table_snapshots")
+        # print(data)
+else:
+    print("Not inserting new table because its the same as the old one")
 
-# insert if they are different
 
-
-logger.info(f"Making snapshot with id {next_snapshot_id}")
-for idx, item in enumerate(table, start=1):
-    data = {}
-    goal_diff = item[1]["goals_for"] - item[1]["goals_against"]
-    item[1]["goal_difference"] = goal_diff
-    data = item[1]
-    data['team_name'] = item[0]
-    data["type"] = 'regular'
-    data["snapshot_id"] = next_snapshot_id
-    insert_row_snapshot(data, "table_snapshots")
-    # print(data)
-    
 conn.close()
